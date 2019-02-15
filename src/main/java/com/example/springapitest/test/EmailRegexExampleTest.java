cd283a7f9171113e6a04806faa51b0b2373d6891
tuanpmt38 <tuanpmt38@gitlab.velacorp.vn>
2019-02-15 11:41:07 +0700

    new code thread / synchronized

--- src/main/java/com/example/springapitest/test/EmailRegexExampleTest.java
+++ src/main/java/com/example/springapitest/test/EmailRegexExampleTest.java
@@ -3,20 +3,20 @@ package com.example.springapitest.test;
 public class EmailRegexExampleTest {
 
     private static EmailRegexExample emailExample;
-    public static final String[] validEmail = new String[] { "a@gmail.com", "ab@yahoo.com", "abc@hotmail.com" };
-    public static final String[] invalidEmail = new String[] { "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
+    public static final String[] validEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invalidEmail = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
 
     public static void main(String args[]) {
 
         emailExample = new EmailRegexExample();
         for (String email : validEmail) {
             boolean isvalid = emailExample.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
         }
         System.out.println();
         for (String email : invalidEmail) {
             boolean isvalid = emailExample.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+            System.out.println("Email is " + email + " is valid: " + isvalid);
         }
     }
 }
