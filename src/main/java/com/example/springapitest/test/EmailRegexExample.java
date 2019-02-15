3f090673420b1bc753dbba20f1a96d28b04663c4
tuanpmt38 <tuanpmt38@gitlab.velacorp.vn>
2019-02-15 11:40:31 +0700

    update ode java core

---
+++ src/main/java/com/example/springapitest/test/EmailRegexExample.java
@@ -0,0 +1,21 @@
+package com.example.springapitest.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailRegexExample {
+
+    private static Pattern pattern;
+    private Matcher matcher;
+    private static final String EMAIL_REGEX= "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+
+    public EmailRegexExample() {
+
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+
+    public boolean validate(String regex){
+        matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
